d45eaa166d308f17b82436fe0b4e1b02947da84c
Denis <den.bugaenko99@gmail.com>
2023-10-10 22:34:09 +0500

    add lesson 1 and 2

---
+++ 1_table/create_table.sql
@@ -0,0 +1,9 @@
+-- создаём таблицу
+CREATE TABLE book (
+    -- автоматически заполняется
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
